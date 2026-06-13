642dd809fcff7951ccb8b2b0a97e8445bda46e04
Esteban G <102876725+Esteban2507@users.noreply.github.com>
2026-06-13 17:28:46 -0300

    Delete langchain_core directory

--- langchain_core/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Minimal langchain_core package stub for prompts and tools."""
-
-from . import prompts
-from . import tools
--- langchain_core/tools.py
+++
@@ -1,8 +0,0 @@
-"""Stubs for tool-related base classes used by agent.py."""
-
-class BaseTool:
-    name: str = "base_tool"
-    description: str = "Base tool"
-
-    def _run(self, *args, **kwargs):
-        raise NotImplementedError()
